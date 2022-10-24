324426f186bc0015e9f2ca2148402e369ddc523c
Jonathan Barsky <barskyjonathan96@gmail.com>
2022-10-24 19:52:48 -0300

    deploy-v3

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
     entry: './src/index.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
-        filename: 'bundle.js',
+        filename: './dist/bundle.js',
         publicPath: '/'
     },
     resolve: {
